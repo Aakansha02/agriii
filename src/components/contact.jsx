0b2730d100ef9e31d1f0137f17c69b0328e90e75
Aakansha02 <87978020+Aakansha02@users.noreply.github.com>
2023-05-26 23:51:01 +0530

    update

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import './contact.css'
+
+
+const submithandler = (event)=>{
+  event.preventDefault();
+  const config={
+
+    SeecureToken:'34ba9862-e645-4500-99bf-c81d5fe019cb ',
+    To : 'akansham392@gmail.com',
+        From : "formstate.email",
+        Subject : "contact form",
+        Body : "body",
+    };
+  if(window.Email){
+    window.Email.send(config).then(()=> alert("email sent succesfully"));
+  }
+}
+
 const ContactUs = () => {
   return (
     <div className="contact">
       <h2>Contact Us</h2>
       {/* <div className="contact-container"> */}
-    
+    <div className="container">
+      <form onSubmit={submithandler}>
+        <h3>GET IN TOUCH</h3>
+        <input type='text' id='name' placeholder='Your name' required></input>
+        <input type='email' id='email' placeholder='Email Id' required></input>
+        <input type='text' id='phone' placeholder='Phone no.' required></input>
+        <textarea id='message' rows="4" placeholder='How can we help you?' required></textarea>
+        <button type='submit'>Send</button>
+      </form>
+    </div>
+  
+
         <div className="contact-info">
           <h3>Address</h3>
           <p>1234 Main Street, City, State, Country</p>
@@ -18,6 +46,7 @@ const ContactUs = () => {
       
        
     </div>
+    
   );
 };
 
